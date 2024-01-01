de9629080a110040f2bb2eb35278f6942352bc5b
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2024-01-02 02:35:34 +0500

    fetch API by use effect

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(PostId, Name, Email, Body) {
+  return { PostId, Name, Email, Body };
+}
 
 export default function Blog() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>Blog</div>
-  )
+    <div>
+      <div>
+        {data && (
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
