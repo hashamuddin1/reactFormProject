9dfd0b64776c757fb6f446c511a7d80e5425d3f1
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2024-01-02 01:22:29 +0500

    static form has been created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import "../App.css";
+import Button from "@mui/material/Button";
 
 export default function Form() {
   return (
-    <div>Form</div>
-  )
+    <div className="formMain">
+      <TextField id="standard-basic" label="First Name" variant="standard" />
+
+      <TextField id="standard-basic" label="Last Name" variant="standard" />
+
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+
+      <TextField id="standard-basic" label="Email Address" variant="standard" />
+
+      <TextField id="standard-basic" label="Password" variant="standard" />
+      <Button variant="contained">Submit</Button>
+    </div>
+  );
 }
